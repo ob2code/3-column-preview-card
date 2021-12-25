75728165094067fdb596d65fb90f98c303e1aa38
Huy Dang <huydhm@gmail.com>
2021-12-25 21:25:06 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bright-orange': '#e38826',
+        'dark-cyan': '#006970',
+        'very-dark-cyan': '#004241',
+        'very-light-gray': '#f2f2f2',
+        'trans-white': '#FFFFFFBD',
+      },
+      fontFamily: {
+        'bigshoulders': ['"Big Shoulders Display"', 'cursive'],
+        'lexend-deca': ['"Lexend Deca"', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
